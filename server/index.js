2261478383edc211c68c3fb3a5edba715624b852
itsJoel59 <joel.j.jansson@gmail.com>
2025-07-31 08:38:04 +0200

    Setup basic server boilerplate

--- server/index.js
+++ server/index.js
@@ -1,5 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+const PORT = 5080;
+
+app.use(cors());
 app.use(express.json());
-app.listen(5080, () => console.log('Server running on port 5080'));
+
+app.get('/', (request, response) => {
+    response.send('Server up and running!');
+});
+
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
